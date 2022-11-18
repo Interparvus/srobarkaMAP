be45ecb8cc18f75ef1a5ba0f58500aefcf23664a
Interparvus <102362917+Interparvus@users.noreply.github.com>
2022-11-18 14:31:30 +0100

    onLICKxd

--- main.js
+++ main.js
@@ -152,6 +152,7 @@ function init() {
   document.body.appendChild(renderer.domElement);
   document.addEventListener("mousemove", onMouseMove, false);
   document.addEventListener("click", onClick);
+  document.addEventListener("touchstart",onClick);
   document.querySelector("#button1").addEventListener("click",btn1);
   document.querySelector("#button2").addEventListener("click",btn2);
 }
