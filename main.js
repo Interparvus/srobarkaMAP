eaef383fe16a74c0ce4bf537580be7a9daea0000
Interparvus <102362917+Interparvus@users.noreply.github.com>
2022-11-19 12:28:30 +0100

    mobile detection

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
 import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.121.1/examples/jsm/controls/OrbitControls.js";
+var isMobile = false; 
+if (
+  /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(
+    navigator.userAgent
+  ) ||
+  /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(
+    navigator.userAgent.substr(0, 4)
+  )
+) {
+  isMobile = true;
+}
 const renderer = new THREE.WebGLRenderer();
 const camera = new THREE.PerspectiveCamera(
   50,
@@ -13,37 +24,38 @@ var mouse,
   raycaster,
   selected = null,
   index,
-  
- element2; 
+  element2;
 let loadData = "./Prizemie.gltf";
-function btn1(){  
-  console.log("pressed!")
+function btn1() {
+  console.log("pressed!");
   scene.remove(Floor);
-  room_id.forEach(cube => {scene.remove(cube)});
-  
+  room_id.forEach((cube) => {
+    scene.remove(cube);
+  });
+
   if (loadData === "./PrvePoschodie.gltf") {
     loadData = "./Prizemie.gltf";
-    ;
   }
-loadGLTF();
-rooms();}
-function btn2(){
+  loadGLTF();
+  rooms();
+}
+function btn2() {
   scene.remove(Floor);
-   room_id.forEach(cube => {scene.remove(cube)});
-  if (loadData ==="./Prizemie.gltf")
-    {loadData = "./PrvePoschodie.gltf";}
-    loadGLTF();
-    rooms();
+  room_id.forEach((cube) => {
+    scene.remove(cube);
+  });
+  if (loadData === "./Prizemie.gltf") {
+    loadData = "./PrvePoschodie.gltf";
+  }
+  loadGLTF();
+  rooms();
 }
 
-  
-  
+function onTouch(event) {
+  event.preventDefault();
+  mouse.x = +(event.targetTouches[0].pageX / window.innerWidth) * 2 + -1;
 
-function onTouch(event){
-    event.preventDefault();
-    mouse.x = +(event.targetTouches[0].pageX / window.innerWidth) * 2 +-1;
-
-    mouse.y = -(event.targetTouches[0].pageY / window.innerHeight) * 2 + 1;
+  mouse.y = -(event.targetTouches[0].pageY / window.innerHeight) * 2 + 1;
 }
 function onMouseMove(event) {
   event.preventDefault();
@@ -57,8 +69,10 @@ function onClick(event) {
   let intersects = raycaster.intersectObjects(room_id, false);
   //raycaster.intersectObjects(element, false);
   if (intersects.length > 0) {
-    room_id.forEach(cube => {cube.material.color.set("#ff3399")});
-    intersects[0].object.material.color.set("yellow")
+    room_id.forEach((cube) => {
+      cube.material.color.set("#ff3399");
+    });
+    intersects[0].object.material.color.set("yellow");
     selected = intersects[0].object;
     index = room.findIndex((x) => x.x_pos === selected.position.x);
     let element = document.getElementById("InfoTabulka");
@@ -69,14 +83,13 @@ function onClick(event) {
         "\n" +
         "Učebňa: " +
         room[index].specific;
-      
 
       console.log(room[index].specific);
     }
   }
 }
-var grid = new THREE.GridHelper(100,100);
-var grid5 = new THREE.GridHelper(100,1000);
+var grid = new THREE.GridHelper(100, 100);
+var grid5 = new THREE.GridHelper(100, 1000);
 
 scene.add(grid);
 //scene.add(grid5);
@@ -168,7 +181,6 @@ function rooms() {
   const geometry = new THREE.BoxGeometry(0.25, 0.25, 0.25);
   const material = new THREE.MeshBasicMaterial({ color: "#ff3399" });
 
-
   for (var i = 0; i < room.length; i++) {
     room_id[i] = new THREE.Mesh(geometry, material);
 
@@ -195,12 +207,13 @@ function init() {
   mouse = new THREE.Vector2();
   raycaster = new THREE.Raycaster();
   document.body.appendChild(renderer.domElement);
-  document.addEventListener("mousemove", onMouseMove, false);
-  document.addEventListener("touchstart", onTouch,false);
-  document.addEventListener("click", onClick);
-  document.addEventListener("touchstart",onClick);
-  document.querySelector("#button1").addEventListener("click",btn1);
-  document.querySelector("#button2").addEventListener("click",btn2);
+  if (isMobile === false)
+{ document.addEventListener("mousemove", onMouseMove, false);
+  document.addEventListener("click", onClick);}
+  document.addEventListener("touchstart", onClick);
+  document.addEventListener("touchstart", onTouch, false);
+  document.querySelector("#button1").addEventListener("click", btn1);
+  document.querySelector("#button2").addEventListener("click", btn2);
 }
 
 function setLight() {
@@ -242,8 +255,6 @@ function animate() {
     newMaterial.transparent = true;
     newMaterial.opacity = 0.5;
     intersects[0].object.material = newMaterial;
-
-    
   }
   renderer.render(scene, camera);
 }
